a667511e4ef06d00ac781af318f99ca966404fa0
EgorovAleksei <plyuh@list.ru>
2024-01-16 02:38:39 +0300

    Add BasketModelViewSet для получения корзины пользователя. Добавили urls router.register(r'baskets', BasketModelViewSet). Создали BasketSerializer для получение корзины в json по get запросу.

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
-from products.models import Product
-from products.serializers import ProductSerializer
-from rest_framework.permissions import IsAdminUser
+from products.models import Product, Basket
+from products.serializers import ProductSerializer, BasketSerializer
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
 # Представление позволяющее применять post, get, delete и т.д. запросы
@@ -19,6 +19,7 @@ class ProductModelViewSet(ModelViewSet):
             self.permission_classes = [IsAdminUser]
         return super(ProductModelViewSet, self).get_permissions()
 
+
 # Просто предоствляет данные. Без возможности удалить, изменить и т.д.
 # domain_name/api/product-list/product_list
 # url path('product-list', ProductListAPIView.as_view(), name='product_list'),
@@ -29,3 +30,16 @@ class ProductModelViewSet(ModelViewSet):
 
 
 # Представление позволяющее применять post, get, delete и т.д. запросы
+
+
+class BasketModelViewSet(ModelViewSet):
+    queryset = Basket.objects.all()
+    serializer_class = BasketSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
+
+    # Создание queryset для конкретного пользователя. Без него возвращает все корзины.
+    def get_queryset(self):
+        queryset = super(BasketModelViewSet, self).get_queryset()
+        return queryset.filter(user=self.request.user)
+
